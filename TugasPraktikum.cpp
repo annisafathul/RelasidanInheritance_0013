89a9a701c183efad5e657efffb85f671841d111e
annisafathul <annisafathul18@gmail.com>
2026-05-11 12:51:26 +0700

    menambahkan kelas member dan showProfile

--- TugasPraktikum.cpp
+++ TugasPraktikum.cpp
@@ -24,3 +24,39 @@ public:
 };
 
 int User::globalId = 0;
+
+
+class Member : public User
+{
+public:
+    bool status;
+
+    Member(string nama, string email, bool status)
+        : User(nama, email)
+    {
+        this->status = status;
+    }
+
+    void showProfile()
+    {
+        cout << "ID     : " << id << endl;
+        cout << "Nama   : " << nama << endl;
+        cout << "Email  : " << email << endl;
+        cout << "Status : ";
+
+        if (status)
+            cout << "Aktif" << endl;
+        else
+            cout << "Nonaktif" << endl;
+    }
+
+    void setStatus(bool status)
+    {
+        this->status = status;
+    }
+
+    bool getStatus()
+    {
+        return status;
+    }
+};
